9d4861545fef13eda7dda8d9b50fcb368340f539
D.saipavan <102216459+Dsaipavan@users.noreply.github.com>
2022-10-12 10:11:09 +0530

    def pr(x):     if x==1:return False     for i in range(2,int(x**0.5)+1):         if x%i==0:return False     return True a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if pr(i):c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,11 @@
-import math
 def pr(x):
     if x==1:return False
-    if x==2:return True
-    for i in range(2,int(math.sqrt(x))+1):
-        if x%i==0:
-            return False
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:return False
     return True
 a=int(input())
 b=int(input())
 c=0
 for i in range(a,b+1):
-    if pr(i):
-        c+=1
+    if pr(i):c+=1
 print(c)
