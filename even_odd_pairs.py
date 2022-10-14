ce1f3e46ca7b71edfa8eee6ab571b762eafcd18f
D.saipavan <102216459+Dsaipavan@users.noreply.github.com>
2022-10-14 22:16:50 +0530

    n=int(input()) x=list(map(int,input().split())) e=[] o=[] z=[] for i in x:     if i%2==0:e.append(i)     else:o.append(i) if len(e)>len(o):     maxi=e else:     maxi=o mn=min(len(e),len(o)) for i in range(mn):     z.append(e[i])     z.append(o[i]) z=z+maxi[mn:] if n%2==0:     print(*z) else:     z.append(0)     print(*z)

--- even_odd_pairs.py
+++ even_odd_pairs.py
@@ -1,18 +1,22 @@
-c=[]
 n=int(input())
 x=list(map(int,input().split()))
-mn=0
-mx=0
-y=[i for i in x if i%2==0]
-z=[i for i in x if i%2==1]
-mn=min(len(y),len(z))
-mx=max(len(y),len(z))
+e=[]
+o=[]
+z=[]
+for i in x:
+    if i%2==0:e.append(i)
+    else:o.append(i)
+if len(e)>len(o):
+    maxi=e
+else:
+    maxi=o
+mn=min(len(e),len(o))
 for i in range(mn):
-    c.append(y[i])
-    c.append(z[i])
-if mx==len(y):d=c+y[mn:]
-if mx==len(z):d=c+z[mn:]
-if n%2==1:
-    d.append(0)
-    print(*d)
-else:print(*d)
+    z.append(e[i])
+    z.append(o[i])
+z=z+maxi[mn:]
+if n%2==0:
+    print(*z)
+else:
+    z.append(0)
+    print(*z)
